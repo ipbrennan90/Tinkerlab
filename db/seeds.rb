3355d62f109c2194b8c5347b85f80b9ed531c1b0
Ian Brennan <ian.brennan@icloud.com>
2015-03-17 16:21:34 -0600

    practicing faker methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@ Project.delete_all
 User.delete_all
 
 category = 3.times.map{Category.create!(name: Faker::Lorem.word)}
-project = 3.times.map{}
+project = Project.create!(title: Faker::Lorem.word, category_id: category[0].id)
+project1 = Project.create!(title: Faker::Lorem.word, category_id: category[1].id)
+project2 = Project.create!(title: Faker::Lorem.word, category_id: category[2].id)
