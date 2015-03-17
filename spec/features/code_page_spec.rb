475632d32ecb62676ef8c2bdb0c5d9ff78e7c628
Ian Brennan <ian.brennan@icloud.com>
2015-03-16 20:56:56 -0600

    working on tests

---
+++ spec/features/code_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails-helper'
+
+feature 'code' do
+
+  scenario 'Code added' do
+    visit root_path
+    click_on "Code"
+    expect(page).to have_content("All projects code")
+    click_on "Add Code"
+    fill_in :codeblock, with: "Test Code"
+    
