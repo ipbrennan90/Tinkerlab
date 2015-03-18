42023582e726b1596e4a6c27ac3705fe3cfc0beb
Ian Brennan <ian.brennan@icloud.com>
2015-03-18 11:04:12 -0600

    added empty posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318165000) do
+ActiveRecord::Schema.define(version: 20150318170355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "posts", force: :cascade do |t|
   end
 
   create_table "projects", force: :cascade do |t|
