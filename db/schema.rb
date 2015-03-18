f39457a826ec6ff19cf4c48eb9ee58fa1be4102e
Ian Brennan <ian.brennan@icloud.com>
2015-03-18 11:06:49 -0600

    re adding user_id column, incorrect spelling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318170501) do
+ActiveRecord::Schema.define(version: 20150318170610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150318170501) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "category_id"
+    t.integer "user_id"
   end
 
   create_table "projects", force: :cascade do |t|
