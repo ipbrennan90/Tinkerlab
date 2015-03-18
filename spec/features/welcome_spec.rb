7a8d5488afc5d1823d84ef56b3688277ec9b346e
Ian Brennan <ian.brennan@icloud.com>
2015-03-18 11:03:32 -0600

    revamped welcome spec, now user not a nil class

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,17 +4,13 @@ feature 'Welcome' do
 
   scenario "Anonymous user can see welcome page with defaults" do
     visit root_path
-    projects = FactoryGirl.create_list(:project, 3)
     users = FactoryGirl.create_list(:user,3)
-    binding.pry
+    projects = FactoryGirl.create_list(:project, 3)
+    #binding.pry
     expect(page).to have_content("TinkerLab")
     expect(page).to have_link("Log in")
     expect(page).to have_link("Sign Up")
-    expect(page).to have_link("#{@user.categories.first.name}")
-    expect(page).to have_link("#{@user.categories.first.projects.first.title}")
-    expect(page).to have_link("#{@user.categories.find(2).name}")
-    expect(page).to have_link("#{@user.categories.find(2).projects.first.title}")
-    expect(page).to have_link("#{@user.categories.find(3).name}")
-    expect(page).to have_link("#{@user.categories.find(3).projects.first.title}")
+    expect(page).to have_link("#{users[0].categories.first.name}")
+    expect(page).to have_link("#{users[0].categories.first.projects.first.title}")
   end
 end
