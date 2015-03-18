d1315e32a356c0eea573c6ea25d2c489b5324c1b
Ian Brennan <ian.brennan@icloud.com>
2015-03-18 11:05:20 -0600

    adjusted posts schema

---
+++ db/migrate/20150318170501_add_categoryid_to_posts.rb
@@ -0,0 +1,5 @@
+class AddCategoryidToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318170355) do
+ActiveRecord::Schema.define(version: 20150318170501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150318170355) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.integer "category_id"
   end
 
   create_table "projects", force: :cascade do |t|
